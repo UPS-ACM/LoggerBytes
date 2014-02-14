70525bc598cfd3fb09c46b4403862fa028a80d28
Billy Rathje <bwjr@nwlink.com>
2014-02-13 16:36:07 -0800

    test

---
+++ test/test/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  test
+//
+//  Created by Billy Rathje on 2/13/14.
+//  Copyright (c) 2014 Billy Rathje. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by Billy Rathje on 2/13/14.
+//  Copyright (c) 2014 Billy Rathje. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
