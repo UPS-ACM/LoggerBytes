75572a17f395a69530b8dfaa684a1a0b555d7321
Chili Johnson <Chili@chilis-mbp.pugetsound.edu>
2014-02-20 16:33:57 -0800

    Legit commit

---
+++ LoggerBytes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoggerBytes
+//
+//  Created by Chili Johnson on 2/13/14.
+//  Copyright (c) 2014 UPS-ACM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
